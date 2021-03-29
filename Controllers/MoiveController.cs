5d21764f30340d472c420d40a8398e28a7beec46
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2021-03-29 12:54:35 +0100

    [MVC] Created a basic MVC pattern [Bootstrap] Added lumen theme

---
+++ Controllers/MoiveController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+namespace Vidly.Controllers
+{
+    public class MoiveController : Controller
+    {
+        // GET: Moive/Random
+        public ActionResult GenerateRandomFilm()
+        {
+            var moive = new Moive() { name = "Default", ID = 1, price = 30 };
+            return View(moive);
+        }
+    }
+}
---
+++ Models/Moive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Moive
+    {
+        public string name { get; set; }
+        public int  price { get; set; }
+        public int ID { get; set; }
+
+    }
+
+    // /Moives/Random
+}
