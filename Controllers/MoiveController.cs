3bbfb6b964cdd6747f4cc52607aa59652c82f432
OphDev <opheliah95@gmail.com>
2021-03-31 18:13:07 +0100

    [Action] Learn about different action results and querying

--- Controllers/MoiveController.cs
+++ Controllers/MoiveController.cs
@@ -8,11 +8,30 @@ namespace Vidly.Controllers
 {
     public class MoiveController : Controller
     {
-        // GET: Moive/Random
+        // changed return type to make unit test easier
         public ActionResult GenerateRandomFilm()
         {
             var moive = new Moive() { name = "Default", ID = 1, price = 30 };
+            //return Content ("Hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy="name" });
             return View(moive);
         }
+
+        public ActionResult Edit (int? id)
+        {
+            return Content("The parameter is: " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if(String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0} and sortBy={1}", pageIndex, sortBy));
+        }
     }
-}
+} 
