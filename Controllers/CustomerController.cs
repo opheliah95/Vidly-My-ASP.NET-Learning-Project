64bb0dc627218957c4fb2c8e276b61b423c4dbce
OphDev <opheliah95@gmail.com>
2021-04-30 15:28:29 +0100

    [Controller] populated a dummy customer list [View] draft function to display customer

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
@@ -11,7 +12,19 @@ namespace Vidly.Controllers
         // GET: Customer
         public ActionResult Index()
         {
+            // populate a dummy list
+            List<Customer> customers = GenerateCustomers();
             return View();
+        }
+
+        // dummy customer data
+        public List<Customer> GenerateCustomers()
+        {
+            List<Customer> c = new List<Customer> {
+                                new Customer { ID = 1, Name = "Doe Joe" },
+                                new Customer { ID = 2, Name = "Jane Smith" },
+                                };
+            return c;
         }
     }
 }
